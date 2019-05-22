775d547096f3496e183d4ff6f655fccb3b2cb664
Dyarc96 <jandyja96@hotmail.com>
2019-05-22 17:26:58 +0200

    Updates

---
+++ precache-manifest.237fd4696e6d66642e8bcfb4e08f1203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d9b8729a53d37c4c84c6057e86e3c9",
+    "url": "/GOTApp/index.html"
+  },
+  {
+    "revision": "61b98ee1ed31240b6315",
+    "url": "/GOTApp/static/css/main.de193db3.chunk.css"
+  },
+  {
+    "revision": "e2d93665a968d12a5fd0",
+    "url": "/GOTApp/static/js/2.a9ff27b7.chunk.js"
+  },
+  {
+    "revision": "61b98ee1ed31240b6315",
+    "url": "/GOTApp/static/js/main.3a3deb65.chunk.js"
+  },
+  {
+    "revision": "df7ddab12b65785a8861",
+    "url": "/GOTApp/static/js/runtime~main.d825a4e9.js"
+  },
+  {
+    "revision": "72cbb9d78dbb18906ecdd3c5ceccda1b",
+    "url": "/GOTApp/static/media/open-book.72cbb9d7.png"
+  },
+  {
+    "revision": "ad946c0a1ffcdc8f294ebdb56dcc6ee4",
+    "url": "/GOTApp/static/media/worldMap.ad946c0a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GOTApp/precache-manifest.3e742e716c540d19413d8d06b940fd2b.js"
+  "/GOTApp/precache-manifest.237fd4696e6d66642e8bcfb4e08f1203.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/GOTApp/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GOTApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
